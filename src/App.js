783fab4263d75ce2894f4396c6459ef593f8811b
hossein_adlll <hosein.adlavaran1382@gmail.com>
2024-09-05 13:05:01 +0330

    some edits

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import Navbar from "./components/navbar";
 
 function App() {
   return (
     <div className="App">
-      
+        <Navbar />
     </div>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+function Navbar(){
+    return(
+        <div>
+            hello world!!
+        </div>
+    )
+}
+
+
+export default Navbar()
