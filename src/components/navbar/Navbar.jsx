fa027c34c3f04485651255c52c294df181cafc3d
hossein_adlll <hosein.adlavaran1382@gmail.com>
2024-09-14 22:31:45 +0330

    some edits

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,29 +1,11 @@
 import styles from './Navbar.module.css'
 
-function Navbar(props) {
-    let title =
-        [
-            {
-                id: 1,
-                title: 'hasan'
-            },
-            {
-                id: 2,
-                title: 'ali'
-            }
-        ]
+function Navbar() {
 
 
     return (
-        <div className={styles.navbar}>
-            <div className={styles.navbarLogoSection}>
-                <img src="src\images\logo.svg" alt="" />
-            </div>
-            <div className={styles.navbarNavigate}>
-            <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-            </div>
+        <div>
+
         </div>
     )
 }
